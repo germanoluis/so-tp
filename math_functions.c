4459758ddf23bdff4432d7cd45c9ffb70503edea
Germano <germanoluis@ufmg.br>
2016-12-12 12:21:16 -0200

    math_functions.c file added and some updating

---
+++ math_functions.c
@@ -0,0 +1,24 @@
+/* Just a random program to sum sin(pi), cos(pi) and
+ * tan(pi) a zillion times */
+
+#include <math.h>
+
+#define PI acos(-1.0)
+
+int main(){
+
+	unsigned long long i;
+	double sine, cosine, tangent;
+	double sum;
+
+	for(i = 0; i < 18446744073; i++){
+		sine = sin(PI);
+		cosine = cos(PI);
+		tangent = tan(PI);
+
+		sum = sine + cosine + tangent;
+	}
+
+	return 0;
+
+}
